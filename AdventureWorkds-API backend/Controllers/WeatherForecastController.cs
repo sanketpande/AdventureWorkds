9f380cbd63b3abfb83fd81453a3ab4857daceb37
sanket pande <snktpande@gmail.com>
2022-09-14 16:43:18 +0530

    added space

--- AdventureWorkds-API backend/Controllers/WeatherForecastController.cs
+++ AdventureWorkds-API backend/Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace AdventureWorkds_API_backend.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
